f52cb8ac51c5a91221e62ac1e9e079d51244d5ac
Eberhard Beilharz <eb1@sil.org>
2017-10-04 18:39:19 +0200

    Add msbuild sample
    Also move BuildDependencyTestApp to Sample
    directory.

---
+++ Samples/MsBuildSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MsBuildSample
+{
+	internal class Program
+	{
+		public static void Main(string[] args)
+		{
+		}
+	}
+}
---
+++ Samples/MsBuildSample/README.md
@@ -0,0 +1,5 @@
+# MsBuild sample
+
+This sample demonstrates how to use the `Dependencies` task. To build this sample:
+
+	xbuild /t:DownloadDependencies build/MsBuildSample.proj
